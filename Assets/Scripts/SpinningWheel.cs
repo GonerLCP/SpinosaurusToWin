a3726b8b6d606e3e1423586100d24525ff9d472e
GonerLCP <119951843+GonerLCP@users.noreply.github.com>
2026-06-20 16:19:56 +0200

    WheelQuiTourne

--- Assets/Scripts/SpinningWheel.cs
+++ Assets/Scripts/SpinningWheel.cs
@@ -1,16 +1,48 @@
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using System.Collections;
 
 public class SpinningWheel : MonoBehaviour
 {
+    Transform Wheeltransform;
+    public float RotationValue;
+    public float RotationDecreaseValue;
+    public float ActualRotationValue;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Wheeltransform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f) // forward
+        {
+            ActualRotationValue += RotationValue;
+            print("up");
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+            ActualRotationValue -= RotationValue;
+            print("down");
+        }
+
+        Wheeltransform.rotation = Wheeltransform.rotation * Quaternion.Euler(0f, 0f, ActualRotationValue);
+
+        if (ActualRotationValue >= RotationDecreaseValue)
+        {
+            ActualRotationValue -= RotationDecreaseValue;
+        }
+        else if (ActualRotationValue <= -RotationDecreaseValue)
+        {
+            ActualRotationValue += RotationDecreaseValue;
+        }
+        else {ActualRotationValue =0f; }
+
+        if (Input.GetKeyDown("space"))
+        {
+            ActualRotationValue = 0f;
+        }
     }
 }
